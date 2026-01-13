7084bb3fee4ad36898c650a9af67ec3765e97e5a
noblefrog96 <68639991+noblefrog96@users.noreply.github.com>
2026-01-14 05:40:35 +0900

    Update constants.js

--- functions/constants.js
+++ functions/constants.js
@@ -39,7 +39,7 @@ exports.FORM_DATA_TEMPLATE = (name, phone) => ({
 });
 */
 
-exports.FORM_URL = 'https://docs.google.com/forms/d/e/1FAIpQLSfAGm17f-zFkhl-1AXMygj-NPKQb7VC9CCst-JaD-h-4_rhSw/formResponse'; // 정식 아산FAP 구글 폼
+exports.FORM_URL = 'https://docs.google.com/forms/d/e/1FAIpQLSelPwd8pmt-wsu_KMVRgoxhzofWDO3Q2JLdmgQvP4UP32XSKA/formResponse'; // 정식 아산FAP 구글 폼
 exports.FORM_DATA_TEMPLATE = (name, phone) => ({
   'entry.1212348438': '케이엔솔(전기)',                // 협력사명
   'entry.513669972': name,                     // 이름
